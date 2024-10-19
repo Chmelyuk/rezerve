56d5a0ceef96a91030ce320d29b25737eebb11de
Chmelyuk <98876964+Chmelyuk@users.noreply.github.com>
2024-10-19 21:52:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -48,3 +48,22 @@ async function startCamera() {
 
 // Обработчик события для кнопки
 document.getElementById('scan-button').addEventListener('click', startCamera);
+
+
+let scrollPosition = 0;
+
+function disableScroll() {
+    scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+    document.body.style.overflow = 'hidden';
+    document.body.style.position = 'fixed';
+    document.body.style.top = `-${scrollPosition}px`;
+    document.body.style.width = '100%';
+}
+
+function enableScroll() {
+    document.body.style.removeProperty('overflow');
+    document.body.style.removeProperty('position');
+    document.body.style.removeProperty('top');
+    document.body.style.removeProperty('width');
+    window.scrollTo(0, scrollPosition);
+}
